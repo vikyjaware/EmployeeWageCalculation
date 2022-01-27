c610399035ea7cdb2ad25cd51dafdd58a2c792f2
vikyjaware <vikyjaware@gmail.com>
2022-01-27 12:21:48 +0530

    [Viky] Add : Folder structure modified

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to employee wage computation program!");
+using EmployeeWageComputation;
+
+Console.WriteLine("Welcome to employee wage computation program!\n");
+
+WageComputation employee = new WageComputation();
+employee.EmpAttendance();
---
+++ EmployeeWageComputation/WageComputation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    public class WageComputation
+    {
+        const int EMP_ABSENT = 0, EMP_PRESENT = 1;
+        public void EmpAttendance()
+        {
+            Random rand = new Random();
+            int EmpCheck=rand.Next(0,2);
+            if (EmpCheck == 0)
+            {
+                Console.WriteLine("Employee is absent "); 
+            }
+            if (EmpCheck==1)
+            {
+                Console.WriteLine("Employee is present ");
+            }
+        }
+
+        
+    }
+}
