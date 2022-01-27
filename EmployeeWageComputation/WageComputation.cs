7997880a919faf0ffebd11f4463de8930f83e374
vikyjaware <vikyjaware@gmail.com>
2022-01-27 16:05:44 +0530

    [Viky] Add : Folder structure added

--- EmployeeWageComputation/WageComputation.cs
+++ EmployeeWageComputation/WageComputation.cs
@@ -8,29 +8,28 @@ namespace EmployeeWageComputation
 {
     public class WageComputation
     {
-        const int EMP_ABSENT = 0, EMP_PRESENT = 1,EMP_PARTTIME_PRESENT=2, WAGE_PER_HR = 20, FULL_DAY_HR = 8, PARTTIME_HR = 4 ;
-        int EmpHrs = 0, EmpTotalWage = 0;
+        const int EMP_ABSENT = 0, EMP_PRESENT = 1,EMP_PARTTIME_PRESENT=2, WAGE_PER_HR = 20, FULL_DAY_HR = 8, PARTTIME_HR = 4, TOTAL_WORKING_DAYS = 20 ;
+        int EmpHrs = 0, EmpTotalWage = 0, i = 0;
         public void EmpPartTimeWage()
         {
+            for (i = 0; i < TOTAL_WORKING_DAYS; i++)
+            {
             Random rand = new Random();
             int EmpCheck = rand.Next(0, 3);
-            switch (EmpCheck)
-            {
+                switch (EmpCheck)
+                {
+                    case EMP_PRESENT:
+                        this.EmpHrs += FULL_DAY_HR;
+                        break;
 
-                case EMP_PRESENT:
-                    this.EmpHrs += FULL_DAY_HR;
-                    Console.WriteLine("Employee is Full time present");
-                    break;
-            
-                case EMP_PARTTIME_PRESENT:
-                    this.EmpHrs += PARTTIME_HR;
-                    Console.WriteLine("Employee is part time present");
-                    break;
+                    case EMP_PARTTIME_PRESENT:
+                        this.EmpHrs += PARTTIME_HR;
+                        break;
 
-                default:
-                    Console.WriteLine("Emplyoee is absent");
-                    this.EmpHrs += 0;
-                    break;
+                    default:
+                        this.EmpHrs += 0;
+                        break;
+                }
             }
             this.EmpTotalWage = EmpHrs * WAGE_PER_HR;
             Console.WriteLine("Employee's total wage is ");
